e25ea0367e80343118e3d5ddb7efe9f74d02507c
nrunyan <nrunyan@unm.edu>
2026-04-19 14:46:04 -0600

    my bad kev

--- Models/Username.cs
+++ Models/Username.cs
@@ -3,10 +3,19 @@ namespace InsecureWebsite.Models
     public class Username
     {
         private static List<Username> usernames = new List<Username>();
-        public required string Name {get;set;}
-        public required string Password {get;set;}
+        public string Name {get;set;}
+        public string Password {get;set;}
         public bool IsAdmin {get;set;} = false; //default value
+
+        public Username(string Name, string Password, bool IsAdmin)
+        {
+            this.Name = Name;
+            this.Password = Password;
+            this.IsAdmin =IsAdmin;
+            
+        }
 
         
     }
+   
 }
