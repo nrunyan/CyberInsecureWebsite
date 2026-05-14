a0b782a0e79f345a8a6daed43b6d622318c984ed
valerie <vbarker@unm.edu>
2026-05-14 14:21:45 -0600

    added descriptions

--- Controllers/HubController.cs
+++ Controllers/HubController.cs
@@ -19,7 +19,7 @@ public class HubController : Controller
         // For each new level add a controller and link it through here
         levels.Add(new Level { Number = 1, 
         Name = "Login", 
-        Description = "asdf",  // Description can be w/e, I was thinking of displaying it somewhere on button hover???
+        Description = "Explore Weak Authentication Vulnerabilities",  // Description can be w/e, I was thinking of displaying it somewhere on button hover???
         Controller = "Level1",
         Action = "Index"}); // Where you want the redirect to point to in your URI
                             // ie "..../Level1/login" then we put "login"
@@ -43,7 +43,7 @@ public class HubController : Controller
 
         levels.Add(new Level { Number = 5,
         Name = "Database Exploitation",
-        Description = "TODO",
+        Description = "Explore the Vulnerabilities of Embedded Vector Databases",
         Controller = "Level1",
         Action = "SearchVulnerability"});
         
